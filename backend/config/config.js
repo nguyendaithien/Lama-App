f6ac50eb1acd1c8722d3352b7d4e5ee3918786fe
hiimDaoKhanhBK <daobakhanhbk@gmail.com>
2022-01-20 09:55:49 +0700

    change config env

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,51 +1,51 @@
-'use strict'
+"use strict";
 
-const env = process.env.NODE_ENV || 'development' // dev or prod
-const util = require('util')
+const env = process.env.NODE_ENV || "development"; // dev or prod
+const util = require("util");
 
 const development = {
-    dbSettings: {
-        host: 'localhost',
-        user: 'duyle',
-        password: 'duyle95',
-        database: 'project_management',
+  dbSettings: {
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASS,
+    database: process.env.SQL_DBNAME,
 
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     },
+  },
 
-    serverSettings: {
-        port: process.env.PORT || 4000
-    }
-}
+  serverSettings: {
+    port: process.env.PORT || 4000,
+  },
+};
 
 const production = {
-    dbSettings: {
-        host: process.env.SQL_HOST,
-        user: process.env.SQL_USER,
-        password: process.env.SQL_PASS,
-        database: process.env.SQL_DBNAME,
+  dbSettings: {
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASS,
+    database: process.env.SQL_DBNAME,
 
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     },
+  },
 
-    serverSettings: {
-        port: process.env.PORT
-    }
-}
+  serverSettings: {
+    port: process.env.PORT,
+  },
+};
 
 const config = {
-    development,
-    production
-}
+  development,
+  production,
+};
 
-module.exports = Object.assign({}, config[env])
+module.exports = Object.assign({}, config[env]);
